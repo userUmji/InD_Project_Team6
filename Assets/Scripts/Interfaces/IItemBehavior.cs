f9dd3b1d8c73b92e2a23d127e93be20a29bbfc06
koexe <aicom5448@gmail.com>
2024-04-02 13:00:55 +0900

    123

--- Assets/Scripts/Interfaces/IItemBehavior.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IItemBehavior 
-{
-    void ExecuteItemEffect(UnitEntity allyUnit, UnitEntity EnemyUnit);
-}
