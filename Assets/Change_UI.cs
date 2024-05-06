c9340968ab33f70d35b3ae5b41ca174917667f9b
UmJi <djawltmd97@gmail.com>
2024-05-07 00:43:54 +0900

    240507_엄지승
    보유 도깨비, 도깨비 캡쳐, 가방, 상세 맵 UI 서로 이동 가능하게 구현

---
+++ Assets/Change_UI.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Change_UI : MonoBehaviour
+{
+    [Header("«•Ω√«ÿ¡Ÿ UI")]
+    public GameObject change_UI;
+
+    public GameObject[] off_UI;
+
+    public static Change_UI instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    public void Change_UI_Button()
+    {
+        for(int i = 0; i < off_UI.Length; i++)
+        {
+            if (off_UI[i].name == "Inventory")
+            {
+                off_UI[i].transform.localScale = new Vector3(0, 0, 1);
+            }
+            else
+            {
+                off_UI[i].SetActive(false);
+            }
+        }
+
+        if (change_UI.name == "Inventory")
+        {
+            change_UI.transform.localScale = new Vector3(1, 1, 1);
+        }
+        else
+        {
+            change_UI.SetActive(true);
+        }
+    }
+
+    public void ALL_OFF_UI() // ∏µÁ UI ≤®¡‹
+    {
+        for (int i = 0; i < off_UI.Length; i++)
+        {
+            if (off_UI[i].name == "Inventory")
+            {
+                off_UI[i].transform.localScale = new Vector3(0, 0, 1);
+            }
+            else
+            {
+                off_UI[i].SetActive(false);
+            }
+        }
+    }
+}
