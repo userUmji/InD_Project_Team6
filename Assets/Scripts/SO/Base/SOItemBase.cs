ea38df9ec79d2089cce2436729ebf4cc28e1a9de
Seoun1 <djsjs123@naver.com>
2024-05-17 20:45:23 +0900

    05/17송준섭
    맵과 미니맵부분 수정하였고 M키를 삭제하고 인벤토리에 있는 상세맵을 열면 플레이어가 다른지역을 이동할때 다른지역으로 이동되게 수정하였습니다
    그리고 가운데 지역에도 포탈기능을 추가하였고 특정 쿼스트까지 진행하면 포탈이 활성화되도록 설정하였습니다

--- Assets/Scripts/SO/Base/SOItemBase.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class SOItemBase : ScriptableObject, IItemBehavior
-{
-    public abstract void ExecuteItemEffect(UnitEntity allyUnit);
-} 
